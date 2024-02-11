f9a6601fa95620ebafe0487143704d9bc960f2c8
Raheem Oluwatobiloba <raim.tobbi@gmail.com>
2024-02-11 20:19:44 +0100

    fix: factorize as many numbers as possible

--- factors.c
+++ factors.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// Function to find and print factorization
 void print_factors(uint64_t n) {
-    uint64_t p, q;
+    // Handle even numbers quickly
+    if (n % 2 == 0) {
+        printf("%lu=2*%lu\n", n, n / 2);
+        return;
+    }
 
-    for (p = 2; p * p <= n; ++p) {
+    // Only iterate through odd numbers starting from 3
+    for (uint64_t p = 3; p * p <= n; p += 2) {
         if (n % p == 0) {
-            q = n / p;
-            printf("%lu=%lu*%lu\n", n, p, q);
+            printf("%lu=%lu*%lu\n", n, p, n / p);
             return;
         }
     }
 
-    // If no factors found, print n=1*n
-    printf("%lu=1*%lu\n", n, n);
+    // If no factors found, print n itself (shouldn't happen with given constraints)
+    printf("%lu=%lu*1\n", n, n);
 }
 
 int main(int argc, char *argv[]) {
